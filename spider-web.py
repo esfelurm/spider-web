8130801ee299a4de4d3646ca78388c03ef96790e
EsfeLurM <104654028+esfelurm@users.noreply.github.com>
2023-08-25 04:42:58 -0700

    Update spider-web.py

--- spider-web.py
+++ spider-web.py
@@ -1,9 +1,18 @@
 import os
+from platform import uname as name
 try:from requests import get
 except:os.system("pip install requests")
 rd, gn, lgn, yw, lrd, be, pe = '\033[00;31m', '\033[00;32m', '\033[01;32m', '\033[01;33m', '\033[01;31m', '\033[00;34m', '\033[01;35m'
 cn = '\033[00;36m'
-
+def clear():
+    if 'Windows' in name():
+        os.system('cls')
+        try:from colorama import init
+        except:os.system("pip install colorama")
+        init()
+    elif 'Linux' in name():
+        os.system('clear')
+clear()
 def banner():
     print (f"""
                                                   
@@ -77,22 +86,22 @@ def sub():
     target = input(f"""
     
 {lrd}+-++-++-++-++-++-++-++-++-++-+
-{rd}|{lgn}s{k}||{lgn}u{k}||{lgn}b{k}||{lgn}d{k}||{lgn}o{k}||{lgn}m{k}||{lgn}a{k}||{lgn}i{k}||{lgn}n{k}||{lgn}s{rd}|
+{rd}|{lgn}s{yw}||{lgn}u{yw}||{lgn}b{yw}||{lgn}d{yw}||{lgn}o{yw}||{lgn}m{yw}||{lgn}a{yw}||{lgn}i{yw}||{lgn}n{yw}||{lgn}s{rd}|
 {lrd}+-++-++-++-++-++-++-++-++-++-+
 		{lrd}  github : {lgn}github.com/esfelurm
 
-{lrd}[{lgn}+{lrd}] {gn}Enter URL Target {cn}[Ex : https://google.com] {gn}: {k}""")     
+{lrd}[{lgn}+{lrd}] {gn}Enter URL Target {cn}[Ex : https://google.com] {gn}: {lgn}""")     
     link_list = open('file/sub.txt', 'r').read().split()
     for List in link_list:
-    	f = target+'/'+List
-		req = get(f)
-		if '404' in req.text: 
-			print (f'\n{lrd}[{yw}NO{lrd}] {lrd}Page not found : {lrd}[ {lrd}{target}/{cn}\033[41m{List}\033[0m{lrd} ]')
-		else: 
-			print (f'\n{lrd}[{lgn}OK{lrd}]{lgn} Page found : {lrd}[ \033[42m{target}/{List}\033[0m{lrd} ]')
+          f = target+'/'+List
+          req = get(f)
+          if '404' in req.text: 
+                print (f'\n{lrd}[{yw}NO{lrd}] {lrd}Page not found : {lrd}[ {lrd}{target}/{cn}\033[41m{List}\033[0m{lrd} ]')
+          else:
+                print (f'\n{lrd}[{lgn}OK{lrd}]{lgn} Page found : {lrd}[ \033[42m{target}/{List}\033[0m{lrd} ]')
                         
 def admin():
-    target = input(f"""{k}
+    target = input(f"""{lgn}
  _______  ______   _______ _________ _       
 (  ___  )(  __  \ (       )\__   __/( (    /|
 | (   ) || (  \  )| () () |   ) (   |  \  ( |
@@ -102,15 +111,15 @@ def admin():
 | )   ( || (__/  )| )   ( |___) (___| )  \  |
 |/     \|(______/ |/     \|\_______/|/    )_)
 
-{lrd}[{lgn}+{lrd}] {gn}Enter URL Target {cn}[Ex : https://google.com] {gn}: {k}""")     
+{lrd}[{lgn}+{lrd}] {gn}Enter URL Target {cn}[Ex : https://google.com] {gn}: {lgn}""")     
     link_list = open('file/admin.txt', 'r').read().split()    
     for List in link_list:
-		    s = target+'/'+List
-            req = get(s)
-			if '404' in req.text: 
-				print (f'\n{lrd}[{yw}NO{lrd}] {lrd}Page not found : {lrd}[ {lrd}{target}/{cn}\033[41m{List}\033[0m{lrd} ]')
-			else: 
-				print (f'\n{lrd}[{lgn}OK{lrd}]{lgn} Page found : {lrd}[ \033[42m{target}/{List}\033[0m{lrd} ]')
+           s = target+'/'+List
+           req = get(s)
+           if '404' in req.text: 
+                        print (f'\n{lrd}[{yw}NO{lrd}] {lrd}Page not found : {lrd}[ {lrd}{target}/{cn}\033[41m{List}\033[0m{lrd} ]')
+           else: 
+                  print (f'\n{lrd}[{lgn}OK{lrd}]{lgn} Page found : {lrd}[ \033[42m{target}/{List}\033[0m{lrd} ]')
 		        	  		    
 
 if Number == '1':

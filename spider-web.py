b09dadc8d9810be98d6f89521fa17162a59b2ac1
EsfeLurM <104654028+esfelurm@users.noreply.github.com>
2023-08-25 14:58:23 +0330

    Add files via upload

---
+++ spider-web.py
@@ -0,0 +1,127 @@
+import os
+try:from requests import get
+except:os.system("pip install requests")
+rd, gn, lgn, yw, lrd, be, pe = '\033[00;31m', '\033[00;32m', '\033[01;32m', '\033[01;33m', '\033[01;31m', '\033[00;34m', '\033[01;35m'
+cn = '\033[00;36m'
+
+def banner():
+    print (f"""
+                                                  
+							 {lrd}                                          
+									 	     :                   
+								   @     	     @                   
+								   @     	@    @-                  
+								  @      	@    @                  
+								 @@      	@@    @@                 
+								@@              @@@    @@                
+							@     *@@                @@@     @        
+						      ##     @@@                  @@@     =@      
+						     @+    @@@@    {cn}.          ` {lrd}    @@@@    :@     
+						    @@   %@@@@      {cn}%        *{lrd}      #@@@@   @@    
+						   .@     *@@@#      {cn}@@%  #@@- {lrd}    +@@@#     @+   
+						   @@        @@@     {cn} @@@#@@      {lrd}@@@.      @@   
+						   @@         :@@#  {cn}#        *{lrd}  -@@+         @@   
+						   @@@@@@@@@@@@%@@% :@@@@@@@@* +@@*@@@@@@@@@@@@   
+						   +@@@@@@@@@@@@@@@. @@@@@@@@  @@@@@@@@@@@@@@@%   
+						      +*-    +@@@@@@ =@@@@@@% @@@@@@*.   -**      
+							     @@@@@@@  @@@@@@  @@@@@@@             
+							  :@@@@@@@@@# @@@@@@ -@@@@@@@@@=          
+							 @@@@@@*@@@@@ =    - #@@@@*@@@@@@.        
+						       %@@@@  +@@@@.           @@@@%  @@@@@       
+						      @@@@   *@@@ {pe}   *@@@@@@%{lrd}    @@@%   @@@@      
+						      @@@   .@@@    {pe}@@@@@@@@@@{lrd}   @@@=   %@@      
+						       @@   @@@    {pe}@@@@-   @@@@{lrd}    @@@   @@.      
+						       #@  @@@    {pe}@@@@@@  @@@@@@{lrd}    @@@  @@       
+							@: @@.    {pe}@@@@@@  @@@@@@.{lrd}    @@  @        
+							@ %@@    {pe}#@@@     .  @@@@{lrd}    @@@ @        
+							@ @@@    {pe}@@@@        @@@@{lrd}    %@@ @        
+							@ @@:    {pe}@@@@ @@  @@ @@@@{lrd}     @@ %        
+						       .  @@     {pe}.@@@@@@  @@@@@@={lrd}     @@  -       
+							  @@@     {pe}@@@@@@  *@@@@@{lrd}     @@@          
+							  @@@@     {pe}@@@@%@@@@@@@:{lrd}    @@@@          
+							    @@@    {pe}:@@@@@@@@@@+{lrd}    @@@            
+							     @@.     {pe}@@@@@@@@{lrd}      @@             
+							      @@       {pe}@@@@{lrd}       @@              
+							      @@        {pe}@@{lrd}        @@              
+							      .@         :        @+              
+							       @                  @.              
+							       @                  %.              
+							      ..                   +              
+							      @                    @              
+							      @                    #              
+												  
+							     *  
+							     
+							     
+						     {lrd}  github : {lgn}github.com/esfelurm
+       
+     						     {lrd}  channel Telegram :{lgn} @esfelurm
+       	     
+{lrd}[{yw}1{lrd}]{lgn} cracker hashing
+{yw}--------------------------
+{lrd}[{yw}2{lrd}]{lgn} Bug hunter
+{yw}--------------------------
+{lrd}[{yw}3{lrd}]{lgn} Admin Panel finder
+{yw}--------------------------
+{lrd}[{yw}4{lrd}]{lgn} subdomain Panel finder
+{yw}--------------------------
+{lrd}[{yw}5{lrd}]{lgn} Reporter Blogfa		
+{yw}--------------------------				
+{lrd}[{yw}6{lrd}]{lgn} cracker Blogfa 		
+
+    """)
+banner()
+Number = input(f"{lrd}[{yw}N{lrd}] {lgn}Enter number : {pe}")
+
+def sub():
+    target = input(f"""
+    
+{lrd}+-++-++-++-++-++-++-++-++-++-+
+{rd}|{lgn}s{k}||{lgn}u{k}||{lgn}b{k}||{lgn}d{k}||{lgn}o{k}||{lgn}m{k}||{lgn}a{k}||{lgn}i{k}||{lgn}n{k}||{lgn}s{rd}|
+{lrd}+-++-++-++-++-++-++-++-++-++-+
+		{lrd}  github : {lgn}github.com/esfelurm
+
+{lrd}[{lgn}+{lrd}] {gn}Enter URL Target {cn}[Ex : https://google.com] {gn}: {k}""")     
+    link_list = open('file/sub.txt', 'r').read().split()
+    for List in link_list:
+    	f = target+'/'+List
+		req = get(f)
+		if '404' in req.text: 
+			print (f'\n{lrd}[{yw}NO{lrd}] {lrd}Page not found : {lrd}[ {lrd}{target}/{cn}\033[41m{List}\033[0m{lrd} ]')
+		else: 
+			print (f'\n{lrd}[{lgn}OK{lrd}]{lgn} Page found : {lrd}[ \033[42m{target}/{List}\033[0m{lrd} ]')
+                        
+def admin():
+    target = input(f"""{k}
+ _______  ______   _______ _________ _       
+(  ___  )(  __  \ (       )\__   __/( (    /|
+| (   ) || (  \  )| () () |   ) (   |  \  ( |
+| (___) || |   ) || || || |   | |   |   \ | |
+|  ___  || |   | || |(_)| |   | |   | (\ \) |	{lrd}  github : {lgn}github.com/esfelurm
+| (   ) || |   ) || |   | |   | |   | | \   |
+| )   ( || (__/  )| )   ( |___) (___| )  \  |
+|/     \|(______/ |/     \|\_______/|/    )_)
+
+{lrd}[{lgn}+{lrd}] {gn}Enter URL Target {cn}[Ex : https://google.com] {gn}: {k}""")     
+    link_list = open('file/admin.txt', 'r').read().split()    
+    for List in link_list:
+		    s = target+'/'+List
+            req = get(s)
+			if '404' in req.text: 
+				print (f'\n{lrd}[{yw}NO{lrd}] {lrd}Page not found : {lrd}[ {lrd}{target}/{cn}\033[41m{List}\033[0m{lrd} ]')
+			else: 
+				print (f'\n{lrd}[{lgn}OK{lrd}]{lgn} Page found : {lrd}[ \033[42m{target}/{List}\033[0m{lrd} ]')
+		        	  		    
+
+if Number == '1':
+    os.system("python src/cracker-hash.py")
+elif Number == '2':
+    os.system("python src/bug.py")
+elif Number == '3':
+    admin()
+elif Number == '4':
+    sub()
+elif Number == '5':
+    os.system("python src/reporter.py")	
+elif Number == '6':
+    os.system("python src/cracker-blogfa.py")
